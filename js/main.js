2dc53c051353a82eb6582741cbeee7ca27e1106f
James Volmert <james.volmert@gmail.com>
2016-07-10 13:08:42 -0600

    radio validation, input validation, and text area validation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
-console.log('sanity check');
 
 function outputUpdate(num) {
     document.querySelector('#output').value = num;
+}
+
+/**
+ *  text area validation
+ */
+
+function textAreaValidation() {
+    
+    var x, text;
+    x = document.getElementById("quote-response").value;
+    
+    if(x === '') {
+        text = "Please enter something";
+    }
+
+    document.getElementById('textAreaError').innerText = text;
+}
+
+/**
+ * input validation
+ */
+function inputValidation() {
+    
+    var x, text;
+    
+    x = document.getElementById('otherConnections').value;
+    if(x === '') {
+        text = 'Please Enter something';
+    }
+    
+    document.getElementById('textAreaError').innerText = text;
+}
+
+/**
+ * radio group validation
+ * need to select one form the radio group
+ **/
+function validateRadio() {
+    var text = 'You must select a value';
+    var radios = document.getElementsByName('information');
+
+    for(var i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+            return true;
+        }
+    }
+
+    document.getElementById('radioError').innerText = text;
+    return false;
 }
